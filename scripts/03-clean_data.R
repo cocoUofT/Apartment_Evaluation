8018df160944254ef68c8d5be50ae3046459b8d8
Coco Liu <cocoliu@Cocos-MacBook-Air.local>
2026-09-25 11:01:11 -0400

    I created the initial version of clean_data, which filtered out unnecessary variables for the study, and keep only the latest evaluation for each building.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,37 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the raw Apartment Building Evaluation data from 2025 to present
+# Author: Kexin Liu
+# Date: 24 September 2026
+# Contact: ws1nn2lj3@gmail.com
+# License: 
+# Pre-requisites: 
+
+# Any other information needed?
 
 #### Workspace setup ####
+
 library(tidyverse)
+raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+variable_interest <- raw_data |>
+  select("RSN", "EVALUATION COMPLETED ON", "CONFIRMED UNITS", "YEAR BUILT", 
+         "PROPERTY TYPE", "TENANT SERVICE REQUEST LOG", "PEST CONTROL LOG",
+         "MAINTENANCE LOG") |>
+  mutate(completion_year = as.integer(
+    format(as.Date(raw_data[["EVALUATION COMPLETED ON"]]), "%Y")
+  ))
+
+sorted <- variable_interest[order(variable_interest$RSN, 
+                                  -variable_interest[["completion_year"]]), ]
+apartment <- sorted[!duplicated(sorted$RSN), ]
+row.names(apartment) <- NULL
+
+# Check for missing value in all columns
+
+colSums(is.na(apartment))
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(apartment, "data/02-analysis_data/analysis_data.csv")
