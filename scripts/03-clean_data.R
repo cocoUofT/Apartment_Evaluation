04b6f2374678a6d13da31d862261637ec69d7bdb
Coco Liu <cocoliu@Cocos-MacBook-Air.local>
2026-09-26 16:08:34 -0400

    I added an additional data frame for analysis relating to the score change between two evaluations for some buildings.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the raw Apartment Building Evaluation data from 2025 to present
+# Purpose: Cleans the raw Apartment Building Evaluation data from 2023 to present
 # Author: Kexin Liu
 # Date: 24 September 2026
 # Contact: ws1nn2lj3@gmail.com
@@ -13,25 +13,45 @@
 library(tidyverse)
 raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 
+
 #### Clean data ####
 
 variable_interest <- raw_data |>
   select("RSN", "EVALUATION COMPLETED ON", "CONFIRMED UNITS", "YEAR BUILT", 
          "PROPERTY TYPE", "TENANT SERVICE REQUEST LOG", "PEST CONTROL LOG",
          "MAINTENANCE LOG") |>
-  mutate(completion_year = as.integer(
+  mutate("YEAR EVALUATED" = as.integer(
     format(as.Date(raw_data[["EVALUATION COMPLETED ON"]]), "%Y")
   ))
 
 sorted <- variable_interest[order(variable_interest$RSN, 
-                                  -variable_interest[["completion_year"]]), ]
+                                  -variable_interest[["YEAR EVALUATED"]]), ]
 apartment <- sorted[!duplicated(sorted$RSN), ]
 row.names(apartment) <- NULL
 
-# Check for missing value in all columns
+colSums(is.na(apartment)) ## Check for missing value in all columns
 
-colSums(is.na(apartment))
 
+#### Additional dataframe for evaluating score change only ####
+
+# Keep buildings that were evaluated more than once
+
+df.history <- raw_data |>
+  select("RSN", "EVALUATION COMPLETED ON", "CONFIRMED UNITS",
+         "TENANT SERVICE REQUEST LOG") |>
+  mutate(completion_date = as.Date(`EVALUATION COMPLETED ON`))
+
+colSums(is.na(df.history)) ## Check for missing value in all columns
+
+scorechange <- df.history |>
+  arrange(RSN, completion_date) |>
+  group_by(RSN) |>
+  summarise(first_date = first(completion_date),
+            latest_date = last(completion_date),
+            first_score = first(`TENANT SERVICE REQUEST LOG`),
+            latest_score = last(`TENANT SERVICE REQUEST LOG`),
+            `CONFIRMED UNITS` = last(`CONFIRMED UNITS`), .groups = "drop")
 
 #### Save data ####
 write_csv(apartment, "data/02-analysis_data/analysis_data.csv")
+write_csv(scorechange, "data/02-analysis_data/score_change_data.csv")
