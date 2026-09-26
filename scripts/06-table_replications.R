44ef6a6ed93a9574145ebd5cd73c988ee7df30bd
Coco Liu <cocoliu@Cocos-MacBook-Air.local>
2026-09-26 12:26:33 -0400

    I created a script for replicating the code for tables used in the paper.

---
+++ scripts/06-table_replications.R
@@ -0,0 +1,10 @@
+#### Preamble ####
+# Purpose: Replicated tables from the paper on Apartment Evaluation
+# Author: Kexin Liu
+# Date: 25 September 2026
+# Contact: ws1nn2lj3@gmail.com
+# License: 
+# Pre-requisites: 
+# The `tidyverse` package must be installed
+# The 'tinytable' package must be installed
+# Any other information needed? 
